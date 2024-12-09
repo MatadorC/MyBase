29f378287a8fad6d556080adb6af88734ec621ca
大川的川 <8052413+the-sichuan-of-dachuan@user.noreply.gitee.com>
2024-12-09 19:14:45 +0800

    修改网络框架

--- app/src/main/java/com/base/mybase/MainActivity.kt
+++ app/src/main/java/com/base/mybase/MainActivity.kt
@@ -4,17 +4,34 @@ import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import com.gov.mybase.net.MyCxHttpCall
 import com.gov.mybase.utils.LogUtils
 import com.gov.mybase.utils.TimeUtils
+import cxhttp.CxHttp
+import cxhttp.CxHttpHelper
+import cxhttp.call.Okhttp3Call
+import cxhttp.converter.JacksonConverter
+import cxhttp.response.body
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import okhttp3.logging.HttpLoggingInterceptor
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val okhttp3Call = Okhttp3Call {
+            callTimeout(15, TimeUnit.SECONDS)
+            addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+        }
+        val jacksonConverter = JacksonConverter()
+        CxHttpHelper.init(scope=MainScope(), debugLog=true, call=okhttp3Call, converter=jacksonConverter)
+
         var tv = findViewById<TextView>(R.id.tv)
 //        GlobalScope.launch {
 //            delay(1000)
@@ -22,6 +39,14 @@ class MainActivity : AppCompatActivity() {
 //
 //        }
         tv.text=TimeUtils.getSupportBeginDayofMonth("2022-11-9")
-
+        val job = CxHttp.get("https://www.baidu.com")
+            //此处可指定协程，不指定默认使用CxHttpHelper.scope
+            .launch { response ->
+                if (response.body != null) {
+                    println("resultGet1: ${response.body}")
+                } else {
+                    // TODO: Can do some exception handling
+                }
+            }
     }
 }
