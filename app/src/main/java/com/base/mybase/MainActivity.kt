1d039f87fd4f1c1c97e507b0fd5192907062b6c2
大川的川 <8052413+the-sichuan-of-dachuan@user.noreply.gitee.com>
2024-12-10 11:06:46 +0800

    优化代码

--- app/src/main/java/com/base/mybase/MainActivity.kt
+++ app/src/main/java/com/base/mybase/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.base.mybase
 
 import android.annotation.SuppressLint
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import com.gov.mybase.net.MyCxHttpCall
 import com.gov.mybase.utils.LogUtils
 import com.gov.mybase.utils.TimeUtils
@@ -20,33 +23,33 @@ import okhttp3.logging.HttpLoggingInterceptor
 import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId")
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val okhttp3Call = Okhttp3Call {
-            callTimeout(15, TimeUnit.SECONDS)
-            addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
-        }
-        val jacksonConverter = JacksonConverter()
-        CxHttpHelper.init(scope=MainScope(), debugLog=true, call=okhttp3Call, converter=jacksonConverter)
-
         var tv = findViewById<TextView>(R.id.tv)
-//        GlobalScope.launch {
-//            delay(1000)
-//            LogUtils.e("eeeeeee"+TimeUtils.getSupportBeginDayofMonth("2022-11-9"))
-//
-//        }
         tv.text=TimeUtils.getSupportBeginDayofMonth("2022-11-9")
-        val job = CxHttp.get("https://www.baidu.com")
-            //此处可指定协程，不指定默认使用CxHttpHelper.scope
-            .launch { response ->
-                if (response.body != null) {
-                    println("resultGet1: ${response.body}")
-                } else {
-                    // TODO: Can do some exception handling
-                }
-            }
     }
+//    @SuppressLint("MissingInflatedId")
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+
+
+//        val okhttp3Call = Okhttp3Call {
+//            callTimeout(15, TimeUnit.SECONDS)
+//            addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+//        }
+//        val jacksonConverter = JacksonConverter()
+//        CxHttpHelper.init(scope=MainScope(), debugLog=true, call=okhttp3Call, converter=jacksonConverter)
+
+//        val job = CxHttp.get("https://www.baidu.com")
+//            //此处可指定协程，不指定默认使用CxHttpHelper.scope
+//            .launch { response ->
+//                if (response.body != null) {
+//                    println("resultGet1: ${response.body}")
+//                } else {
+//                    // TODO: Can do some exception handling
+//                }
+//            }
+//    }
 }
